defc890edb15bbc19b4c54970608815ade23ed96
Cyclone1070 <128883620+Cyclone1070@users.noreply.github.com>
2026-07-08 17:57:23 +1000

    clean slate

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FeedModule } from './features/feed/feed.module';
-
-@Module({
-  imports: [FeedModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
--- src/features/feed/feed.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FeedService } from './feed.service';
-import { RedditProvider } from './providers/reddit-provider.service';
-
-@Module({
-  providers: [RedditProvider, FeedService],
-  exports: [FeedService],
-})
-export class FeedModule {}
